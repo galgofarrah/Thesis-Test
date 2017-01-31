a3d3e355c87f3ea03c9727763036398b60774254
galgofarrah <2013001140@my.xu.edu.ph>
2017-01-31 11:04:56 +0800

    Slider

---
+++ Slider/app/src/main/java/com/example/sony/slider/IntroScreen.java
@@ -0,0 +1,13 @@
+package com.example.sony.slider;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IntroScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro_screen);
+    }
+}
